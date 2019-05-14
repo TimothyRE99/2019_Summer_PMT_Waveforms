e71947426dbd722d698f7cc3d847ae67e0040436
timothyre99 <timothyre99@gmail.com>
2019-05-14 14:29:18 -0400

    Changed datareadtest.py to display as array

--- datareadtest.py
+++ datareadtest.py
@@ -2,14 +2,17 @@
 import csv
 from itertools import islice
 
-#setting variable
+#setting variables
 x = 0
+results = []
 
 #opening and printing data file
 while x < 10:
     with open('g:/data/watchman/20190514_watchman_spe/C2--waveforms--0000'+str(x)+'.txt','r')as f:
         data = csv.reader(f)
         for row in islice(data, 10):
-            print(row)
+            results.append(row)
+    print(results)
     print()
+    results = []
     x += 1
