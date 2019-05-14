ab63f3e0e91ab384952c163edc39deb777061c75
timothyre99 <timothyre99@gmail.com>
2019-05-14 14:25:47 -0400

    Changing datareadtest.py to print multiple

--- datareadtest.py
+++ datareadtest.py
@@ -1,7 +1,15 @@
+#import necessary
 import csv
 from itertools import islice
 
-with open('g:/data/watchman/20190514_watchman_spe/C2--waveforms--00000.txt','r')as f:
-    data = csv.reader(f)
-    for row in islice(data, 10):
-        print(row)
+#setting variable
+x = 0
+
+#opening and printing data file
+while x < 10:
+    with open('g:/data/watchman/20190514_watchman_spe/C2--waveforms--0000'+str(x)+'.txt','r')as f:
+        data = csv.reader(f)
+        for row in islice(data, 10):
+            print(row)
+    print()
+    x += 1
