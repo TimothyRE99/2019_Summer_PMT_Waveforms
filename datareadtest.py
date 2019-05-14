3330cd6e554767cc5d1e9c02e6f930322930c07c
timothyre99 <timothyre99@gmail.com>
2019-05-14 14:44:53 -0400

    datareadtest.py made more flexible to user input

--- datareadtest.py
+++ datareadtest.py
@@ -3,16 +3,22 @@ import csv
 from itertools import islice
 
 #setting variables
-x = 0
+counter = 0
+print("Insert Number of Files Here:")
+num_files = int(input())
+print("Insert Number of Rows Here:")
+num_rows = int(input())
+
+#setting arrays
 results = []
 
 #opening and printing data file
-while x < 10:
-    with open('g:/data/watchman/20190514_watchman_spe/C2--waveforms--0000'+str(x)+'.txt','r')as f:
+while counter < num_files:
+    with open('g:/data/watchman/20190514_watchman_spe/C2--waveforms--'+str(counter).zfill(5)+'.txt','r')as f:
         data = csv.reader(f)
-        for row in islice(data, 10):
+        for row in islice(data, 4, num_rows):
             results.append(row)
     print(results)
     print()
     results = []
-    x += 1
+    counter += 1
