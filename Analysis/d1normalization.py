6ed2c3b7890cc5b0dd4974d730074c99d7a1b75a
timothyre99 <timothyre99@gmail.com>
2019-05-15 16:29:20 -0400

    added normalization function d1normalization.py

---
+++ Analysis/d1normalization.py
@@ -0,0 +1,19 @@
+#Normalize d1 csv's so peak = 1 and baseline = 0
+
+#import necessary
+import numpy as np
+import os
+from readwaveform import read_waveform as rw
+from writewaveform import write_waveform
+
+#define constants
+Nloops = len(os.listdir('G:/data/watchman/20190514_watchman_spe/d1/d1_50centered'))
+numhead = 5
+
+#normalization
+for i in range(Nloops):
+    filename = 'G:/data/watchman/20190514_watchman_spe/d1/d1_50centered/D1--waveforms--%05d.txt' % i
+    writename = 'G:/data/watchman/20190514_watchman_spe/d1/d1_normalized/D1--waveforms--%05d.txt' % i
+    (t,y,header) = rw(filename,numhead)
+    y_norm = y/min(y[370:1370])
+    write_waveform(t,y_norm,writename,header)
