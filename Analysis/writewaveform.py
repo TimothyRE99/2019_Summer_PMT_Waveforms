38f3d3833a9fe66adc4f480977a6d6b13ee8f3d0
timothyre99 <timothyre99@gmail.com>
2019-05-15 11:57:18 -0400

    Created writewaveform.py

---
+++ Analysis/writewaveform.py
@@ -0,0 +1,30 @@
+#writing a waveform
+
+#import necessary
+import sys
+sys.path.append('../')
+sys.path.append('')
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+#Write the data
+def write_waveform(x,y,filename,header):
+    fileout = open(filename,'w')
+    for entry in header:
+        fileout.write(entry)
+    for ix,iy in zip(x,y):
+        line = '%.7E,%f\n' % (ix,iy)
+        fileout.write(line)
+    fileout.close()
+
+#For testing
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(prog="write waveform",description="write a waveform datafile.")
+    parser.add_argument("--header",type=str,help='header string for the output file',default=['LECROYWaveRunner‘N,20113,Waveform\n', 'Segments,1,SegmentSize,4002\n', 'Segment,TrigTime,TimeSinceSegment1\n', '#1,14-May-2019 13:29:30,0                 \n', 'Time,Ampl\n'])
+    parser.add_argument("--filename",type=str,help="filename",default="./C2--waveforms--00000.txt")
+    args = parser.parse_args()
+    x = range(1000)
+    y = [np.random.normal(0,1.) for i in range(1000)]
+    write_waveform(x,y,args.filename,args.header)
