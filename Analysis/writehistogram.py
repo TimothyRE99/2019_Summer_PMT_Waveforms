422558c91c43d316bc972189217671adebe2d6da
timothyre99 <timothyre99@gmail.com>
2019-05-16 11:16:16 -0400

    Created writehistogram.py

---
+++ Analysis/writehistogram.py
@@ -0,0 +1,22 @@
+#Takes histogram data and writes it to a file
+
+#Import necessary
+import numpy as np
+
+#write the data
+def write_histogram(value,filename):
+    histogram = open(filename,"a")
+    histogram.write(value+",")
+    histogram.close
+    return
+
+#testing
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(prog="write histogram",description="write a histogram datafile.")
+    parser.add_argument("--filename",type=str,help="filename",default="./C2--histograms.txt")
+    args = parser.parse_args()
+    length = np.random.randint(10,100)
+    for i in range(length):
+        value = str(i)
+        write_histogram(value,args.filename)
