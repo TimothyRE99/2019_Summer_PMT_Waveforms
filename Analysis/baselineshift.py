0c70893b84199026fdc3c25487e4db98ac5a93fa
timothyre99 <timothyre99@gmail.com>
2019-05-20 12:13:47 -0400

    added argparse

--- Analysis/baselineshift.py
+++ Analysis/baselineshift.py
@@ -7,12 +7,22 @@ from writewaveform import write_waveform
 import os
 
 #shift baseline
-Nloops = len(os.listdir('G:/data/watchman/20190516_watchman_spe/d1/d1_renamed'))
-numhead = 5
-for i in range(Nloops):
-    filename = 'G:/data/watchman/20190516_watchman_spe/d1/d1_renamed/D1--waveforms--%05d.txt' % i
-    writename = 'G:/data/watchman/20190516_watchman_spe/d1/d1_baseline_shifted/D1--waveforms--%05d.txt' % i
-    (t,y,header) = rw(filename,numhead)
-    baseline = np.mean(y[0:200])                            #determining average value at beginning to establish baseline
-    y_new = (y - baseline)                                  #reducing y to factor out baseline
-    write_waveform(t,y_new,writename,header)
+def baselineshift(data_date,numhead):
+    Nloops = len(os.listdir('G:/data/watchman/'+data_date+'_watchman_spe/d1/d1_renamed'))
+    for i in range(Nloops):
+        filename = 'G:/data/watchman/'+data_date+'_watchman_spe/d1/d1_renamed/D1--waveforms--%05d.txt' % i
+        writename = 'G:/data/watchman/'+data_date+'_watchman_spe/d1/d1_baseline_shifted/D1--waveforms--%05d.txt' % i
+        (t,y,header) = rw(filename,numhead)
+        baseline = np.mean(y[0:200])                            #determining average value at beginning to establish baseline
+        y_new = (y - baseline)                                  #reducing y to factor out baseline
+        write_waveform(t,y_new,writename,header)
+
+if __name__ == '__main__':
+    import argparse
+        import argparse
+    parser = argparse.ArgumentParser(prog='baselineshift', description='Removing baseline from data')
+    parser.add_argument('--data_date',type = str,help = 'date when data was gathered, YYYYMMDD', default = '20190516')
+    parser.add_argument('--numhead',type=int,help='number of lines to ignore for header',default = 5)
+    args = parser.parse_args()
+
+    baselineshift(args.data_date,args.numhead)
