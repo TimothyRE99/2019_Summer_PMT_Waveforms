9987dca1450513019a8483f27937245631716359
timothyre99 <timothyre99@gmail.com>
2019-05-21 13:28:13 -0400

    Added explanatory comments

--- Analysis/waveform_viewer.py
+++ Analysis/waveform_viewer.py
@@ -9,15 +9,15 @@ import os
 
 #cycle through
 def waveform_viewer(data_date,subfolder_name,numhead):
-    NLoops = len(os.listdir('G:/data/watchman/'+data_date+'_watchman_spe/d1/'+subfolder_name))
-    files = os.listdir('G:/data/watchman/'+data_date+'_watchman_spe/d1/'+subfolder_name)
-    for i in range(NLoops):
-        filename = 'G:/data/watchman/'+data_date+'_watchman_spe/d1/'+subfolder_name+'/'+files[i]
-        (x,y,_) = rw(filename,numhead)
-        print(filename)
-        plt.plot(x,y)
-        plt.title(files[i])
-        plt.show()
+    NLoops = len(os.listdir('G:/data/watchman/'+data_date+'_watchman_spe/d1/'+subfolder_name))          #determine number of files in directory
+    files = os.listdir('G:/data/watchman/'+data_date+'_watchman_spe/d1/'+subfolder_name)                #create list of file names in directory
+    for i in range(NLoops):                                                                             #cycle through number of files
+        filename = 'G:/data/watchman/'+data_date+'_watchman_spe/d1/'+subfolder_name+'/'+files[i]        #open first file in list
+        (x,y,_) = rw(filename,numhead)                                                                  #get data from file, ignore header
+        print(filename)                                                                                 #print name of file
+        plt.plot(x,y)                                                                                   #plot data from file
+        plt.title(files[i])                                                                             #title plot with name of file
+        plt.show()                                                                                      #show plot, with pause in code
 
 if __name__ == '__main__':
     import argparse
