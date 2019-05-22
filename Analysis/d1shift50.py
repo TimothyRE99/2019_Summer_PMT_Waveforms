6e6d74c09f449ba5d69d5657058a3670a3487430
timothyre99 <timothyre99@gmail.com>
2019-05-22 12:31:28 -0400

    Fixed misplaced parenthesis

--- Analysis/d1shift50.py
+++ Analysis/d1shift50.py
@@ -19,7 +19,7 @@ def center_check(Nloops,numhead,data_date):
         index_50 = int(index[0])                                #converting to integer
         center_list = np.append(center_list,index_50)           #appending index location to list of index locations
     np.sort(center_list)                                        #sort center_list array
-    max_index = int(round(center_list[len((center_list)-1)]))   #determine maximum index
+    max_index = int(round(center_list[(len(center_list)-1)]))   #determine maximum index
     min_index = int(round(center_list[0]))                      #determine minimum index
     center_index = int(round(np.mean(center_list)))             #establishing mean index location, rounding it, and converting to integer
     return(max_index,min_index,center_index)
