90e4894e2a6a2cfc139921569dcb24769eaa86dc
timothyre99 <timothyre99@gmail.com>
2019-05-22 12:05:18 -0400

    Generalized formula

--- Analysis/readhistogram.py
+++ Analysis/readhistogram.py
@@ -11,12 +11,9 @@ def read_histogram(filename, x_label, title, savename, data_date, histo_mean, hi
     fin = open(filename,'r')
     for line in fin:
         histo = np.append(histo, float(line.split(',')[0]))
-    np.sort(histo)
-    min_x = histo[0]
-    max_x = histo[(len(histo)-1)]
-    x_axis = np.arange(min_x,max_x,0.00000000001)
+    x_axis = np.linspace(histo_mean - 3*histo_std, hist_mean + 3*histo_std, 1000)
     fig = plt.figure(figsize=(6,4))
-    plt.plot(x_axis, norm.pdf(x_axis,hist_mean,histo_std))
+    plt.plot(x_axis, norm.pdf(x_axis,histo_mean,histo_std))
     plt.hist(histo, bins=250)                               #set histogram to divide contents into 50 bins
     plt.xlabel(x_label)                                     #set x-axis label
     plt.ylabel("count")                                     #set y-axis label
