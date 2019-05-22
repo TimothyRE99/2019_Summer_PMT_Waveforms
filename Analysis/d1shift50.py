7d2f0c77471d3e633f0f0c33ee876eb2131e4a2f
timothyre99 <timothyre99@gmail.com>
2019-05-22 11:26:35 -0400

    Fixed to slice off problem indices

--- Analysis/d1shift50.py
+++ Analysis/d1shift50.py
@@ -18,14 +18,19 @@ def center_check(Nloops,numhead,data_date):
         index = [k for k, x in enumerate(check) if x]           #code to enumerate index values
         index_50 = int(index[0])                                #converting to integer
         center_list = np.append(center_list,index_50)           #appending index location to list of index locations
+    np.sort(center_list)                                        #sort center_list array
+    max_index = int(round(center_list[len((center_list)-1)]))   #determine maximum index
+    min_index = int(round(center_list[0]))                      #determine minimum index
     center_index = int(round(np.mean(center_list)))             #establishing mean index location, rounding it, and converting to integer
-    return(center_index)
+    return(max_index,min_index,center_index)
 
 #shifting to align
 def d1shift50(data_date,numhead):
     Nloops = len(os.listdir('G:/data/watchman/'+data_date+'_watchman_spe/d1/d1_baseline_shifted'))
-    center_index = center_check(Nloops,numhead,data_date)
+    (max_index,min_index,center_index) = center_check(Nloops,numhead,data_date)
+    print(min_index)
     print(center_index)
+    print(max_index)
     for i in range(Nloops):
         filename = 'G:/data/watchman/'+data_date+'_watchman_spe/d1/d1_baseline_shifted/D1--waveforms--%05d.txt' % i
         writename = 'G:/data/watchman/'+data_date+'_watchman_spe/d1/d1_50centered/D1--waveforms--%05d.txt' % i
@@ -36,8 +41,14 @@ def d1shift50(data_date,numhead):
         index_50 = int(index[0])
         t_50 = t[index_50]
         t_new = (t - t_50)                                          #shifting t_50 to t=0s
-        t_50centered = np.roll(t_new, center_index - index_50)      #rolling data vectors to align along common point
-        y_50centered = np.roll(y, center_index - index_50)
+        #rolling so 50 rising point is at center index
+        t_intermediate = np.roll(t_new,center_index - index_50)
+        y_intermediate = np.roll(y,center_index - index_50)
+        first_cutoff = (center_index - min_index)                   #determining latest first index
+        last_cutoff = (len(y) - max_index + center_index)           #determining earliest last index
+        #slicing off 'problem indices'
+        t_50centered = t_intermediate[first_cutoff:last_cutoff]
+        y_50centered = y_intermediate[first_cutoff:last_cutoff]
         write_waveform(t_50centered, y_50centered, writename, header)
 
 if __name__ == '__main__':
