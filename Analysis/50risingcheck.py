f818de4853d59e824e5d5a701ff77abcb656355a
timothyre99 <timothyre99@gmail.com>
2019-05-24 17:51:20 -0400

    Checks location of rising 50%

---
+++ Analysis/50risingcheck.py
@@ -0,0 +1,15 @@
+import numpy as np
+from readwaveform import read_waveform as rw
+from writewaveform import write_waveform
+import os
+
+Nloops = len(os.listdir('G:/data/watchman/20190516_watchman_spe/d1/d1_baseline_shifted'))
+for i in range(Nloops):
+    filename = 'G:/data/watchman/20190516_watchman_spe/d1/d1_baseline_shifted/D1--waveforms--%05d.txt' % i
+    (_,y,_) = rw(filename,5)
+    y_norm = y/min(y[370:1370])                             #normalizing to make calculations easier
+    check = y_norm >= 0.5                                   #50% crossing criteria
+    index = [k for k, x in enumerate(check) if x]           #code to enumerate index values
+    index50 = int(index[0])
+    if index50 < 370:
+        print(str(index50)+', '+str(filename))
