Fixed calculation of 50% points

Analysis/determineFWHM.py
@@ -17,10 +17,15 @@ def determine(data_date,numhead):
         filename = 'G:/data/watchman/'+data_date+'_watchman_spe/d1/d1_baseline_shifted/D1--waveforms--%05d.txt' % i
         (t,y,_) = rw(filename,numhead)
         y_norm = y/min(y[370:1370])
-        check = y_norm >= .5                                #determining where 50% falling and rising are located
+        check = y_norm <= .5                                #determining where 50% falling and rising are located
+        check_peak = y_norm = 1
+        index_peak = [k for k, x in enumerate(check_peak) if x]
+        peak_index = int(index_peak[0])
         index = [k for k, x in enumerate(check) if x]
-        index_first = int(index[0])
-        index_last = int(index[len(index)-1])
+        index_low = index[np.where(index < peak_index)]
+        index_high = index[np.where(index > peak_index)]
+        index_first = int(index_low[len(index_low)-1])
+        index_last = int(index_high[0])
         FWHM = str(t[index_last] - t[index_first])          #FWHM is time at falling 50% - time at rising 50%
         wh(FWHM,writename)
     #create histogram from saved file