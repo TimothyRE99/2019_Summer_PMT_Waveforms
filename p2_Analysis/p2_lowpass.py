b81e2699850b4d88d188ad38f93efc261a6566d8
timothyre99 <timothyre99@gmail.com>
2019-05-31 16:04:02 -0400

    Added main function to view individual waveforms

--- p2_Analysis/p2_lowpass.py
+++ p2_Analysis/p2_lowpass.py
@@ -2,6 +2,8 @@
 
 #import necessary
 import numpy as np
+import matplotlib.pyplot as plt
+from readwaveform import read_waveform as rw
 
 #low pass filter
 def lpfFirstOrder(v,tau,fsps):
@@ -13,3 +15,19 @@ def lpfFirstOrder(v,tau,fsps):
         else:
             y[i] = v[i]*alpha + (1-alpha)*y[i-1]
     return y
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(prog="p2 lowpass",description="applies lowpass filter to waveform.")
+    parser.add_argument("--numhead",type=int,help='number of header lines to skip in the raw file',default=5)
+    parser.add_argument("--filename",type=str,help="filename",default='g:/data/watchman/20190516_watchman_spe/d2/d2_raw/D2--waveforms--00000.txt')
+    parser.add_argument("--tau",type=str,help="tau value",default=0.00000001)
+    parser.add_argument("--fsps",type=str,help="hz, samples/s",default=20000000000)
+    args = parser.parse_args()
+
+    t,v,header = rw(args.filename,args.numhead)
+    y = lpfFirstOrder(v,args.tau,args.fsps)
+    print(header)
+    plt.plot(t,v,color = 'blue')
+    plt.plot(t,y,color = 'red')
+    plt.show()
