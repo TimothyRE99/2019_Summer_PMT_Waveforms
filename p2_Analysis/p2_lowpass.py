c584fdc6d2dd93ef4b2aaf5dddae74973474b1b3
timothyre99 <timothyre99@gmail.com>
2019-05-31 16:05:55 -0400

    Updated default arguments to be floats

--- p2_Analysis/p2_lowpass.py
+++ p2_Analysis/p2_lowpass.py
@@ -21,8 +21,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog="p2 lowpass",description="applies lowpass filter to waveform.")
     parser.add_argument("--numhead",type=int,help='number of header lines to skip in the raw file',default=5)
     parser.add_argument("--filename",type=str,help="filename",default='g:/data/watchman/20190516_watchman_spe/d2/d2_raw/D2--waveforms--00000.txt')
-    parser.add_argument("--tau",type=str,help="tau value",default=0.00000001)
-    parser.add_argument("--fsps",type=str,help="hz, samples/s",default=20000000000)
+    parser.add_argument("--tau",type=float,help="tau value",default=0.000000005)
+    parser.add_argument("--fsps",type=float,help="hz, samples/s",default=20000000000.0)
     args = parser.parse_args()
 
     t,v,header = rw(args.filename,args.numhead)
