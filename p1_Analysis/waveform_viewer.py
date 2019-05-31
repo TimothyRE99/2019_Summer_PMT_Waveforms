3b6e9ccdc3df4d193be61a13de4c9004ab391eb0
timothyre99 <timothyre99@gmail.com>
2019-05-31 15:35:19 -0400

    Updated to have sorted()

--- p1_Analysis/waveform_viewer.py
+++ p1_Analysis/waveform_viewer.py
@@ -9,10 +9,10 @@ import os
 
 #cycle through
 def waveform_viewer(datadate,subfolder_name,numhead):
-    NLoops = len(os.listdir('G:/data/watchman/'+datadate+'_watchman_spe/d1/'+subfolder_name))          #determine number of files in directory
-    files = os.listdir('G:/data/watchman/'+datadate+'_watchman_spe/d1/'+subfolder_name)                #create list of file names in directory
+    NLoops = len(os.listdir('G:/data/watchman/'+datadate+'_watchman_spe/d1/'+subfolder_name))           #determine number of files in directory
+    files = sorted(os.listdir('G:/data/watchman/'+datadate+'_watchman_spe/d1/'+subfolder_name))         #create list of file names in directory
     for i in range(NLoops):                                                                             #cycle through number of files
-        filename = 'G:/data/watchman/'+datadate+'_watchman_spe/d1/'+subfolder_name+'/'+files[i]        #open first file in list
+        filename = 'G:/data/watchman/'+datadate+'_watchman_spe/d1/'+subfolder_name+'/'+files[i]         #open first file in list
         (x,y,_) = rw(filename,numhead)                  #get data from file, ignore header
         print(filename)                                 #print name of file
         plt.plot(x,y)                                   #plot data from file
