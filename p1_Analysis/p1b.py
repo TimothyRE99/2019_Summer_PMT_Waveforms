5fdc48794064d377c129ef5c839655d1901a51bc
timothyre99 <timothyre99@gmail.com>
2019-05-31 10:51:08 -0400

    Updated to have more checks

--- p1_Analysis/p1b.py
+++ p1_Analysis/p1b.py
@@ -8,13 +8,14 @@ from writewaveform import write_waveform
 import matplotlib.pyplot as plt
 
 #sorting out double SPEs
-def p1b_sort(datadate,charge_mean,peak_mean,numhead):
+def p1b_sort(datadate,charge_mean,peak_mean,FWHM_mean,numhead):
     #set file locations for charge and peak histograms
     charge_name = 'G:/data/watchman/'+datadate+'_watchman_spe/d1/d1_histograms/charge.txt'
     peak_name = 'G:/data/watchman/'+datadate+'_watchman_spe/d1/d1_histograms/peak_amplitude.txt'
     #initialize histogram arrays
     charge_histo = np.array([])
     peak_histo = np.array([])
+    FWHM_histo = np.array([])
     #read histogram txt files int othe arrays
     charge_fin = open(charge_name,'r')
     for line in charge_fin:
@@ -24,11 +25,17 @@ def p1b_sort(datadate,charge_mean,peak_mean,numhead):
     for line in peak_fin:
         peak_histo = np.append(peak_histo, float(line.split(',')[0]))
     peak_fin.close
+    FWHM_fin = open(peak_name,'r')
+    for line in FWHM_fin:
+        FWHM_histo = np.append(peak_histo, float(line.split(',')[0]))
+    FWHM_fin.close
     #setting up arrays listing indices of where the value is twice the mean or greater
     charge_doubles = np.asarray(charge_histo >= 2*charge_mean).nonzero()
     charge_doubles = charge_doubles[0]
     peak_doubles = np.asarray(peak_histo >= 2*peak_mean).nonzero()
     peak_doubles = peak_doubles[0]
+    FWHM_doubles = np.asarray(FWHM_histo >= 2*FWHM_mean).nonzero()
+    FWHM_doubles = FWHM_doubles[0]
     for i in range(len(charge_histo)):
         print(i)
         #setting location of files to read
@@ -37,9 +44,20 @@ def p1b_sort(datadate,charge_mean,peak_mean,numhead):
         writename_double = 'G:/data/watchman/'+datadate+'_watchman_spe/d1/d1_final_doubles/D1--waveforms--%05d.txt' % i
         writename_single = 'G:/data/watchman/'+datadate+'_watchman_spe/d1/d1_final_spes/D1--waveforms--%05d.txt' % i
         t,v,header = rw(filename,numhead)                                   #reading data from waveform file
-        if np.any(charge_doubles == i) and np.any(peak_doubles == i):       #checking if both charge and peak are greater than twice the mean
+        if (np.any(charge_doubles == i) and np.any(peak_doubles == i)) or (np.any(FWHM_doubles == i) and np.any(charge_doubles == i)):       #checking if both charge and peak are greater than twice the mean
             print("Was double!")
             write_waveform(t,v,writename_double,header)
+        elif any([np.any(charge_doubles == i), np.any(peak_doubles == i), np.any(FWHM_doubles == i)]):
+            plt.plot(t,v)
+            plt.show()
+            double_check = 'Initialization.'
+            while double_check != 's' and double_check != 'd':
+                double_check = input('Is there a single or double? "s" or "d"\n')
+            if double_check == 's':
+                write_waveform(t,v,writename_single,header)
+            if double_check == 'd':
+                print("Was double!")
+                write_waveform(t,v,writename_double,header)
         else:
             write_waveform(t,v,writename_single,header)
     return
@@ -50,8 +68,9 @@ if __name__ == '__main__':
     parser.add_argument('--datadate',type = str,help = 'date when data was gathered, YYYYMMDD', default = '20190516')
     parser.add_argument('--charge_mean',type = float,help = 'mean calculated from charge histogram plot', default = 1.6799e-12)
     parser.add_argument('--peak_mean',type = float,help = 'mean calculated from peak histogram plot', default = 0.0064251)
+    parser.add_argument('--FWHM_mean',type = float,help = 'mean calculated from peak histogram plot', default = 7.9859e-9)
     parser.add_argument('--numhead',type = int,help = 'number of lines to skip for header', default = 5)
     args = parser.parse_args()
 
-    p1b_sort(args.datadate,args.charge_mean,args.peak_mean,args.numhead)
+    p1b_sort(args.datadate,args.charge_mean,args.peak_mean,args.FWHM_mean,args.numhead)
     info_file(args.datadate)                    #generate d1_info.txt file
