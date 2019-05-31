0c607d1d63c4a9f0c691a55f1d9a4bcd9c33b5ea
timothyre99 <timothyre99@gmail.com>
2019-05-31 17:17:12 -0400

    Made defaults in scientific notation for easy use

--- p2_Analysis/p2_lowpass.py
+++ p2_Analysis/p2_lowpass.py
@@ -22,8 +22,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog="p2 lowpass",description="applies lowpass filter to waveform.")
     parser.add_argument("--numhead",type=int,help='number of header lines to skip in the raw file',default=5)
     parser.add_argument("--filename",type=str,help="filename",default='g:/data/watchman/20190516_watchman_spe/d2/d2_raw/D2--waveforms--00000.txt')
-    parser.add_argument("--tau",type=float,help="tau value",default=0.000000005)
-    parser.add_argument("--fsps",type=float,help="hz, samples/s",default=20000000000.0)
+    parser.add_argument("--tau",type=float,help="tau value",default=2e-8)
+    parser.add_argument("--fsps",type=float,help="hz, samples/s",default=20e9)
     args = parser.parse_args()
 
     t,v,header = rw(args.filename,args.numhead)
