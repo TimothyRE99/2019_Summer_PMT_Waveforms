69b4a1a7eb9b42da949e6cc3a49ffceb5187e932
timothyre99 <timothyre99@gmail.com>
2019-06-05 13:05:48 -0400

    Reworked to be generalized internally

--- p2_Analysis/p2_lpf_plotting.py
+++ p2_Analysis/p2_lpf_plotting.py
@@ -5,10 +5,10 @@ from matplotlib import pyplot as plt
 from readwaveform import read_waveform as rw
 
 #plotting the waveforms
-(t_single,v_single,_) = rw('g:/data/watchman/20190516_watchman_spe/d2/d2_average.txt',1)
-(t_double,v_double,_) = rw('g:/data/watchman/20190516_watchman_spe/d2/d2_average_doubled.txt',1)
-(t_quadruple,v_quadruple,_) = rw('g:/data/watchman/20190516_watchman_spe/d2/d2_average_quadrupled.txt',1)
-(t_octuple,v_octuple,_) = rw('g:/data/watchman/20190516_watchman_spe/d2/d2_average_octupled.txt',1)
+(t_single,v_single,_) = rw('g:/data/watchman/20190516_watchman_spe/d2/d2_raw/D2--waveforms--00000.txt',5)
+(t_double,v_double,_) = rw('g:/data/watchman/20190516_watchman_spe/d2/d2_rise_doubled/D2--waveforms--00000.txt',5)
+(t_quadruple,v_quadruple,_) = rw('g:/data/watchman/20190516_watchman_spe/d2/d2_rise_quadrupled/D2--waveforms--00000.txt',5)
+(t_octuple,v_octuple,_) = rw('g:/data/watchman/20190516_watchman_spe/d2/d2_rise_octupled/D2--waveforms--00000.txt',5)
 fig = plt.figure(figsize=(6,4))
 plt.plot(t_single,v_single)
 plt.plot(t_double,v_double,color='red')
