2a2baa16be57f3c70ef2c5258e427b4bca0ce4d2
timothyre99 <timothyre99@gmail.com>
2019-06-05 13:17:24 -0400

    Made into "movie" of graphs

--- p2_Analysis/p2_lpf_plotting.py
+++ p2_Analysis/p2_lpf_plotting.py
@@ -3,16 +3,19 @@
 #import necessary
 from matplotlib import pyplot as plt
 from readwaveform import read_waveform as rw
+import os
 
 #plotting the waveforms
-(t_single,v_single,_) = rw('g:/data/watchman/20190516_watchman_spe/d2/d2_raw/D2--waveforms--00000.txt',5)
-(t_double,v_double,_) = rw('g:/data/watchman/20190516_watchman_spe/d2/d2_rise_doubled/D2--waveforms--00000.txt',5)
-(t_quadruple,v_quadruple,_) = rw('g:/data/watchman/20190516_watchman_spe/d2/d2_rise_quadrupled/D2--waveforms--00000.txt',5)
-(t_octuple,v_octuple,_) = rw('g:/data/watchman/20190516_watchman_spe/d2/d2_rise_octupled/D2--waveforms--00000.txt',5)
-fig = plt.figure(figsize=(6,4))
-plt.plot(t_single,v_single)
-plt.plot(t_double,v_double,color='red')
-plt.plot(t_quadruple,v_quadruple,color='green')
-plt.plot(t_octuple,v_octuple,color='purple')
-plt.show()
-fig.savefig('G:/data/watchman/20190516_watchman_spe/d2/d2_images/average_lpfing_multiple.png',dpi = 2500)
+Nloops = len(os.listdir('G:/data/watchman/20190516_watchman_spe/d2/d2_raw')) - 1
+for i in range(Nloops):
+    (t_single,v_single,_) = rw('g:/data/watchman/20190516_watchman_spe/d2/d2_raw/D2--waveforms--%05d.txt' % i,5)
+    (t_double,v_double,_) = rw('g:/data/watchman/20190516_watchman_spe/d2/d2_rise_doubled/D2--waveforms--%05d.txt' % i,5)
+    (t_quadruple,v_quadruple,_) = rw('g:/data/watchman/20190516_watchman_spe/d2/d2_rise_quadrupled/D2--waveforms--%05d.txt' % i,5)
+    (t_octuple,v_octuple,_) = rw('g:/data/watchman/20190516_watchman_spe/d2/d2_rise_octupled/D2--waveforms--%05d.txt' % i,5)
+    #fig = plt.figure(figsize=(6,4))
+    plt.plot(t_single,v_single)
+    plt.plot(t_double,v_double,color='red')
+    plt.plot(t_quadruple,v_quadruple,color='green')
+    plt.plot(t_octuple,v_octuple,color='purple')
+    plt.show()
+    #fig.savefig('G:/data/watchman/20190516_watchman_spe/d2/d2_images/average_lpfing_multiple.png',dpi = 2500)
