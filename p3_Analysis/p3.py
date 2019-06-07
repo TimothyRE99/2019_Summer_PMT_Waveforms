1b240f983030ffccfdf462b097523d2a617cf10c
timothyre99 <timothyre99@gmail.com>
2019-06-07 11:55:42 -0400

    Added explanatory comments

--- p3_Analysis/p3.py
+++ p3_Analysis/p3.py
@@ -10,18 +10,21 @@ from writewaveform import write_waveform
 
 #digitizing
 def digitize(v,noise):
-    v_new = (v * (2**14 - 1)*2 + 0.5)
-    noise_array = np.random.normal(loc=0.0, scale = noise, size = len(v_new))
-    v_final = np.add(v_new, noise_array)
+    v_new = (v * (2**14 - 1)*2 + 0.5)       #multiplying by digitizer formula to convert to bits
+    noise_array = np.random.normal(loc=0.0, scale = noise, size = len(v_new))       #generating noise array
+    v_final = np.add(v_new, noise_array)    #adding noise to digitized values
+    v_final = v_final.astype(int)           #converting values to ints
     return(v_final)
 
 #downsampling
 def downsampling(t,v,fsps,new_fsps):
-    steps = fsps/new_fsps
-    start_index = random.randint(0,steps - 1)
-    multiplier = math.floor((len(v) - start_index - 1) / steps)
+    steps = fsps/new_fsps                                           #determining how many 'steps' from original waveform can be taken
+    start_index = random.randint(0,steps - 1)                       #randomly determining start index within range [0,steps)
+    multiplier = math.floor((len(v) - start_index - 1) / steps)     #determining max amount you can multiply the steps value by and add onto the start value without overshooting end of array
+    #initializing arrays
     t_new = np.array([])
     v_new = np.array([])
+    #grabbing only values of array that the digitizer would grab
     for i in range(multiplier + 1):
         t_new = np.append(t_new,start_index + i * steps)
         v_new = np.append(v_new,start_index + i * steps)
@@ -29,6 +32,7 @@ def downsampling(t,v,fsps,new_fsps):
 
 #reading and writing waveforms and calling other functions
 def p3(noise_filter,noise,datadate,numhead,fsps,new_fsps):
+    #establishing directory names
     if noise_filter == 0:
         filedir1 = 'g:/data/watchman/'+datadate+'_watchman_spe/d3/d3_raw/'
         filedir2 = 'g:/data/watchman/'+datadate+'_watchman_spe/d3/d3_rise_doubled/'
@@ -47,6 +51,7 @@ def p3(noise_filter,noise,datadate,numhead,fsps,new_fsps):
         writedir2 = 'g:/data/watchman/'+datadate+'_watchman_spe/d3/d3_rise_doubled_analyzed_noise=' + str(noise) + 'V/'
         writedir4 = 'g:/data/watchman/'+datadate+'_watchman_spe/d3/d3_rise_quadrupled_analyzed_noise=' + str(noise) + 'V/'
         writedir8 = 'g:/data/watchman/'+datadate+'_watchman_spe/d3/d3_rise_octupled_analyzed_noise=' + str(noise) + 'V/'
+    #creating directories if they don't exist
     if not os.path.exists(writedir1):
         os.makedirs(writedir1)
     if not os.path.exists(writedir2):
@@ -55,9 +60,10 @@ def p3(noise_filter,noise,datadate,numhead,fsps,new_fsps):
         os.makedirs(writedir4)
     if not os.path.exists(writedir8):
         os.makedirs(writedir8)
-    Nloops = len(os.listdir(filedir2))
+    Nloops = len(os.listdir(filedir2))      #establishing how many files to cycle through
     for i in range(Nloops):
-        print('File: %05d' % i)
+        print('File: %05d' % i)             #printing number of file currently being processed
+        #establishing read and write names
         filename1 = filedir1 + 'D3--waveforms--%05d.txt' % i
         filename2 = filedir2 + 'D3--waveforms--%05d.txt' % i
         filename4 = filedir4 + 'D3--waveforms--%05d.txt' % i
@@ -66,18 +72,22 @@ def p3(noise_filter,noise,datadate,numhead,fsps,new_fsps):
         writename2 = writedir2 + 'D3--waveforms--%05d.txt' % i
         writename4 = writedir4 + 'D3--waveforms--%05d.txt' % i
         writename8 = writedir8 + 'D3--waveforms--%05d.txt' % i
+        #reading in waveform values
         (t1,v1,header1) = rw(filename1,numhead)
         (t2,v2,header2) = rw(filename2,numhead)
         (t4,v4,header4) = rw(filename4,numhead)
         (t8,v8,header8) = rw(filename8,numhead)
+        #downsampling waveform values
         (t_down_1,v_down_1) = downsampling(t1,v1,fsps,new_fsps)
         (t_down_2,v_down_2) = downsampling(t2,v2,fsps,new_fsps)
         (t_down_4,v_down_4) = downsampling(t4,v4,fsps,new_fsps)
         (t_down_8,v_down_8) = downsampling(t8,v8,fsps,new_fsps)
+        #digitizing waveform values
         v_digit_1 = digitize(v_down_1,noise)
         v_digit_2 = digitize(v_down_2,noise)
         v_digit_4 = digitize(v_down_4,noise)
         v_digit_8 = digitize(v_down_8,noise)
+        #saving downsampled and digitized waveforms
         write_waveform(t_down_1,v_digit_1,writename1,header1)
         write_waveform(t_down_2,v_digit_2,writename2,header2)
         write_waveform(t_down_4,v_digit_4,writename4,header4)
--- p3_Analysis/p3_prelim.py
+++ p3_Analysis/p3_prelim.py
@@ -6,6 +6,7 @@ import shutil
 
 #moving files
 def p2_prelim(datadate,noise):
+    #setting up directories to read from and write to
     if noise == 0:
         filedir1 = 'g:/data/watchman/'+datadate+'_watchman_spe/d2/d2_raw/'
         filedir2 = 'g:/data/watchman/'+datadate+'_watchman_spe/d2/d2_rise_doubled/'
@@ -24,7 +25,8 @@ def p2_prelim(datadate,noise):
         writedir2 = 'g:/data/watchman/'+datadate+'_watchman_spe/d3/d3_rise_doubled_noise=' + str(noise) + 'V/'
         writedir4 = 'g:/data/watchman/'+datadate+'_watchman_spe/d3/d3_rise_quadrupled_noise=' + str(noise) + 'V/'
         writedir8 = 'g:/data/watchman/'+datadate+'_watchman_spe/d3/d3_rise_octupled_noise=' + str(noise) + 'V/'
-    files1 = sorted(os.listdir(filedir1))
+    files1 = sorted(os.listdir(filedir1))           #creating and sorting list of files from raw directory to be able to take out the info file
+    #creating paths if they don't exist
     if not os.path.exists(writedir1):
         os.makedirs(writedir1)
     if not os.path.exists(writedir2):
@@ -33,25 +35,31 @@ def p2_prelim(datadate,noise):
         os.makedirs(writedir4)
     if not os.path.exists(writedir8):
         os.makedirs(writedir8)
+    #cycling through files in raw directory
     for i in range(len(files1)):
-        print('Unfiltered File: %05d' % i)
-        filename1 = filedir1 + files1[i]
+        print('Unfiltered File: %05d' % i)      #showing count of file being processed
+        filename1 = filedir1 + files1[i]        #establishing name of file
+        #taking out info file to prevent naming issues
         if files1[i] == 'd1_info.txt':
             writename1 = writedir1 + 'd2_info.txt'
         else:
             writename1 = writedir1 + 'D3--waveforms--%05d.txt' % i
-        shutil.copy2(filename1,writename1)
+        shutil.copy2(filename1,writename1)      #writing files to new directory
     for i in range(len(files1) - 1):
-        print('Filtered File: %05d' % i)
+        print('Filtered File: %05d' % i)        #showing count of file being processed
+        #establishing names of files
         filename2 = filedir2 + 'D2--waveforms--%05d.txt' % i
         filename4 = filedir4 + 'D2--waveforms--%05d.txt' % i
         filename8 = filedir8 + 'D2--waveforms--%05d.txt' % i
+        #establishing names to write to
         writename2 = writedir2 + 'D3--waveforms--%05d.txt' % i
         writename4 = writedir4 + 'D3--waveforms--%05d.txt' % i
         writename8 = writedir8 + 'D3--waveforms--%05d.txt' % i
+        #writing file to new directory
         shutil.copy2(filename2,writename2)
         shutil.copy2(filename4,writename4)
         shutil.copy2(filename8,writename8)
+
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(prog='p3 prelim', description='moving files from d2 to d3')
