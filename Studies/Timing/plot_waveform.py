694c22e1a4203fcb90d281ac102352095e9f4e53
timothyre99 <timothyre99@gmail.com>
2019-06-11 11:33:36 -0400

    Created to plot CFD waveforms

---
+++ Studies/Timing/plot_waveform.py
@@ -0,0 +1,34 @@
+#opens waveform and plots it
+
+#import necessary
+import numpy as np
+import matplotlib.pyplot as plt
+from readwaveform import read_waveform as rw
+import os
+
+#plotting loop function
+def plot_waveform(datadate,numhead,subfolder,n_box,n_shift,n_mult,ZCL):
+    filedir = 'G:/data/watchman/'+datadate+'_watchman_spe/studies/timing/nbox='+str(n_box)+'_nshift='+str(n_shift)+'_nmult='+str(n_mult)+'/'+subfolder+'/'
+    Nloops = len(os.listdir(filedir))
+    for i in range(Nloops):
+        filename = filedir + 'D3--waveforms--%05d.txt' % i
+        print(filename)
+        (t,v,_) = rw(filename,numhead)
+        plt.plot(t,v)
+        plt.axhline(y=0,color='black')
+        plt.get_current_fig_manager().window.showMaximized()
+        plt.show()
+
+#main function
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(prog="plot waveform",description="cycles through CFD waveforms and plots them.")
+    parser.add_argument('--datadate',type = str,help = 'date when data was gathered, YYYYMMDD', default = '20190516')
+    parser.add_argument('--numhead',type=int,help='number of lines to ignore for header',default = 5)
+    parser.add_argument('--subfolder',type = str,help = 'how much the rise time was altered', default = 'raw')
+    parser.add_argument('--n_box',type=int,help='n value for boxcar averager',default = 0)
+    parser.add_argument('--n_shift',type=int,help='number of indices to shift inverted waveform',default = 1)
+    parser.add_argument('--n_mult',type=int,help='amount to multiply base waveform by, must be power of 2',default = 1)
+    args = parser.parse_args()
+
+    plot_waveform(args.datadate,args.numhead,args.subfolder,args.n_box,args.n_shift,args.n_mult)
