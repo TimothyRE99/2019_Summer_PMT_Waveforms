178683a8519e4de5af0199d79eae1c3077faea28
timothyre99 <timothyre99@gmail.com>
2019-06-13 10:12:57 -0400

    Reset to default

--- Studies/Timing/plot_waveform.py
+++ Studies/Timing/plot_waveform.py
@@ -11,7 +11,7 @@ import os
 def plot_waveform(datadate,numhead,subfolder,n_box,n_shift,n_mult):
     filedir = 'G:/data/watchman/'+datadate+'_watchman_spe/studies/timing/nbox='+str(n_box)+'/nshift='+str(n_shift)+'/nmult='+str(n_mult)+'/'+subfolder+'/'
     Nloops = len(os.listdir(filedir))
-    for i in range(2414,Nloops):
+    for i in range(Nloops):
         filename = filedir + 'D3--waveforms--%05d.txt' % i
         print(filename)
         (t,v,_) = rw(filename,numhead)
@@ -35,9 +35,9 @@ if __name__ == '__main__':
     parser.add_argument('--datadate',type = str,help = 'date when data was gathered, YYYYMMDD', default = '20190516')
     parser.add_argument('--numhead',type=int,help='number of lines to ignore for header',default = 5)
     parser.add_argument('--subfolder',type = str,help = 'how much the rise time was altered', default = 'raw')
-    parser.add_argument('--n_box',type=int,help='n value for boxcar averager',default = 2)
+    parser.add_argument('--n_box',type=int,help='n value for boxcar averager',default = 0)
     parser.add_argument('--n_shift',type=int,help='number of indices to shift inverted waveform',default = 1)
-    parser.add_argument('--n_mult',type=int,help='amount to multiply base waveform by, must be power of 2',default = 4)
+    parser.add_argument('--n_mult',type=int,help='amount to multiply base waveform by, must be power of 2',default = 1)
     args = parser.parse_args()
 
     plot_waveform(args.datadate,args.numhead,args.subfolder,args.n_box,args.n_shift,args.n_mult)
