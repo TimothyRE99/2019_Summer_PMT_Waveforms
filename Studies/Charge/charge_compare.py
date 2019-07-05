9573031bad227fc986a509fc37a7881790314066
Timothy Emeigh <timothyre99@gmail.com>
2019-07-05 10:29:06 -0400

    created comment outline for function

---
+++ Studies/Charge/charge_compare.py
@@ -0,0 +1,11 @@
+#Calculates charge for waveform and corresponding downsampled and digitized waveform and compares
+
+#import necessary
+
+#calculates charge
+
+#converting standard waveform to digitized, no noise, no donwsampling
+
+#calling functions
+
+#main function
