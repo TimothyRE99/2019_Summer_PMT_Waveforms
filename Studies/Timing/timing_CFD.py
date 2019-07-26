b321a01b0a902411db84ff1a3471beee0c29c93c
Timothy Emeigh <timothyre99@gmail.com>
2019-07-26 14:37:11 -0400

    Prepped for eliza's files

--- Studies/Timing/timing_CFD.py
+++ Studies/Timing/timing_CFD.py
@@ -47,7 +47,7 @@ def sum_wf(v_att,v_delay):
 #calls other functions
 def timing_CFD(datadate,numhead,subfolder,n_box,n_delay,n_att,samplerate):
     #establishes directory to write to
-    writedir = 'G:/data/watchman/'+datadate+'_watchman_spe/studies/timing/' + samplerate + 'nbox='+str(n_box)+'/ndelay='+str(n_delay)+'/natt='+str(n_att)+'/'+subfolder+'/'
+    writedir = 'G:/data/watchman/'+datadate+'_watchman_spe/studies/timing/' + samplerate + '/nbox='+str(n_box)+'/ndelay='+str(n_delay)+'/natt='+str(n_att)+'/'+subfolder+'/'
     #creates write directory if needed
     if not os.path.exists(writedir):
         os.makedirs(writedir)
@@ -71,23 +71,23 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(prog="timing CFD",description="Applies CFD algorithm to prepare for ZCF.")
     parser.add_argument('--datadate',type = str,help = 'date when data was gathered, YYYYMMDD', default = '20190724')
-    parser.add_argument('--numhead',type=int,help='number of lines to ignore for header',default = 1)
+    parser.add_argument('--numhead',type=int,help='number of lines to ignore for header',default = 5)
     parser.add_argument('--subfolder',type = str,help = 'how much the rise time was altered', default = 'raw_gained')
     parser.add_argument('--samplerate',type = str,help = 'downsampled rate to analyze (1 Gsps, 500 Msps, 250 Msps, 125 Msps)',default = '1 Gsps')
     args = parser.parse_args()
 
     #cycles through each combination of n values
-    #for n_box in range(5):
-    #    if n_box == 3:
-    #        pass
-    #    else:
-    #        for n_delay in range(1,17):
-    #            if n_delay != 1 and n_delay != 2 and n_delay != 4 and n_delay != 8 and n_delay !=  16:
-    #                pass
-    #            else:
-    #                for n_att in range(1,5):
-    #                    if n_att == 3:
-    #                        pass
-    #                    else:
-    #                        timing_CFD(args.datadate,args.numhead,args.subfolder,n_box,n_delay,n_att,args.samplerate)
-    timing_CFD(args.datadate,args.numhead,args.subfolder,2,1,2,args.samplerate)
+    for n_box in range(5):
+        if n_box == 3:
+            pass
+        else:
+            for n_delay in range(1,17):
+                if n_delay != 1 and n_delay != 2 and n_delay != 4 and n_delay != 8 and n_delay !=  16:
+                    pass
+                else:
+                    for n_att in range(1,5):
+                        if n_att == 3:
+                            pass
+                        else:
+                            timing_CFD(args.datadate,args.numhead,args.subfolder,n_box,n_delay,n_att,args.samplerate)
+    #timing_CFD(args.datadate,args.numhead,args.subfolder,2,1,2,args.samplerate)
