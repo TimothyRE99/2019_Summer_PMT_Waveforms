743737b946e6b50c067bba2c770b844b0a8413f4
Timothy Emeigh <timothyre99@gmail.com>
2019-08-02 11:23:59 -0400

    Updated for new trigger study method

--- Studies/Trigger/trigger_study.py
+++ Studies/Trigger/trigger_study.py
@@ -47,62 +47,22 @@ def third_checker(datadate,numhead,mean,std_noise,subfolder,new_fsps,samplerate,
     return(true_positives,Nloops,noise_rate,percent_above)
 
 #checking against 1/4 mean peak
-def fourth_checker(datadate,numhead,mean,std_noise,subfolder,new_fsps,samplerate,mean_noise,std):
+def fourth_checker(datadate,mean,std_noise,subfolder,new_fsps,samplerate,mean_noise,std):
     threshold = mean / 4                        #establishes threshold
     Nloops = len(os.listdir('G:/data/watchman/'+datadate+'_watchman_spe/d3/' + samplerate + '/d3_'+subfolder+'_analyzed/'))    #establishes number of files to cycle through
-    for i in range(Nloops):
-        print('File Number, One Fourth Peak = %05d' % i)     #prints what file you're on
-        #establishes and creates directories if needed
-        true_pos_dir = 'G:/data/watchman/'+datadate+'_watchman_spe/studies/trigger/' + samplerate + '/fourth/'+subfolder+'/true_positives'
-        false_neg_dir = 'G:/data/watchman/'+datadate+'_watchman_spe/studies/trigger/' + samplerate + '/fourth/'+subfolder+'/false_negatives'
-        if not os.path.exists(true_pos_dir):
-            os.makedirs(true_pos_dir)
-        if not os.path.exists(false_neg_dir):
-            os.makedirs(false_neg_dir)
-        #establishes file to read from and reads
-        filename = 'G:/data/watchman/'+datadate+'_watchman_spe/d3/' + samplerate + '/d3_'+subfolder+'_analyzed/D3--waveforms--%05d.txt' % i
-        (_,v,_) = rw(filename,numhead)
-        #flips and checks against threshold
-        v = -1 * v
-        v_peak = max(v)
-        if v_peak >= threshold:
-            shutil.copy2(filename,true_pos_dir)
-        else:
-            shutil.copy2(filename,false_neg_dir)
-    true_positives = len(os.listdir(true_pos_dir))
     prob_happening_noise = gauss_check(std_noise,threshold,mean_noise)
     noise_rate = prob_happening_noise * new_fsps
     percent_above = 1 - gauss_check(std,threshold,mean)
-    return(true_positives,Nloops,noise_rate,percent_above)
+    return(Nloops,noise_rate,percent_above)
 
 #checking against 1/6 mean peak
-def sixth_checker(datadate,numhead,mean,std_noise,subfolder,new_fsps,samplerate,mean_noise,std):
+def sixth_checker(datadate,mean,std_noise,subfolder,new_fsps,samplerate,mean_noise,std):
     threshold = mean / 6                        #establishes threshold
     Nloops = len(os.listdir('G:/data/watchman/'+datadate+'_watchman_spe/d3/' + samplerate + '/d3_'+subfolder+'_analyzed/'))    #establishes number of files to cycle through
-    for i in range(Nloops):
-        print('File Number, One Sixth Peak = %05d' % i)     #prints what file you're on
-        #establishes and creates directories if needed
-        true_pos_dir = 'G:/data/watchman/'+datadate+'_watchman_spe/studies/trigger/' + samplerate + '/sixth/'+subfolder+'/true_positives'
-        false_neg_dir = 'G:/data/watchman/'+datadate+'_watchman_spe/studies/trigger/' + samplerate + '/sixth/'+subfolder+'/false_negatives'
-        if not os.path.exists(true_pos_dir):
-            os.makedirs(true_pos_dir)
-        if not os.path.exists(false_neg_dir):
-            os.makedirs(false_neg_dir)
-        #establishes file to read from and reads
-        filename = 'G:/data/watchman/'+datadate+'_watchman_spe/d3/' + samplerate + '/d3_'+subfolder+'_analyzed/D3--waveforms--%05d.txt' % i
-        (_,v,_) = rw(filename,numhead)
-        #flips and checks against threshold
-        v = -1 * v
-        v_peak = max(v)
-        if v_peak >= threshold:
-            shutil.copy2(filename,true_pos_dir)
-        else:
-            shutil.copy2(filename,false_neg_dir)
-    true_positives = len(os.listdir(true_pos_dir))
     prob_happening_noise = gauss_check(std_noise,threshold,mean_noise)
     noise_rate = prob_happening_noise * new_fsps
     percent_above = 1 - gauss_check(std,threshold,mean)
-    return(true_positives,Nloops,noise_rate,percent_above)
+    return(Nloops,noise_rate,percent_above)
 
 #main function
 if __name__ == '__main__':
@@ -120,16 +80,16 @@ if __name__ == '__main__':
     (mean,std) = det(args.datadate,args.numhead,args.subfolder,args.samplerate)
 
     (third_true_positives, third_Nloops, third_noise_rate, third_percent_above) = third_checker(args.datadate,args.numhead,mean,args.std_noise,args.subfolder,args.new_fsps,args.samplerate,args.mean_noise,std)
-    (fourth_true_positives, fourth_Nloops, fourth_noise_rate, fourth_percent_above) = fourth_checker(args.datadate,args.numhead,mean,args.std_noise,args.subfolder,args.new_fsps,args.samplerate,args.mean_noise,std)
-    (sixth_true_positives, sixth_Nloops, sixth_noise_rate, sixth_percent_above) = sixth_checker(args.datadate,args.numhead,mean,args.std_noise,args.subfolder,args.new_fsps,args.samplerate,args.mean_noise,std)
+    (fourth_Nloops, fourth_noise_rate, fourth_percent_above) = fourth_checker(args.datadate,mean,args.std_noise,args.subfolder,args.new_fsps,args.samplerate,args.mean_noise,std)
+    (sixth_Nloops, sixth_noise_rate, sixth_percent_above) = sixth_checker(args.datadate,mean,args.std_noise,args.subfolder,args.new_fsps,args.samplerate,args.mean_noise,std)
 
     print('One Third Mean Peak Gives:\n\t' + str(third_true_positives) + '/' + str(third_Nloops) + ' True Positives')
-    print('One Fourth Mean Peak Gives:\n\t' + str(fourth_true_positives) + '/' + str(fourth_Nloops) + ' True Positives')
-    print('One Sixth Mean Peak Gives:\n\t' + str(sixth_true_positives) + '/' + str(sixth_Nloops) + ' True Positives')
     print('One Third Mean Peak Gives: %05d Files Predicted' % int(third_percent_above*third_Nloops))
     print('One Fourth Mean Peak Gives: %05d Files Predicted' % int(fourth_percent_above*fourth_Nloops))
     print('One Sixth Mean Peak Gives: %05d Files Predicted' % int(sixth_percent_above*sixth_Nloops))
 
     dark_rate = ['%05g Hertz' % third_noise_rate, '%05g Hertz' % fourth_noise_rate, '%05g Hertz' % sixth_noise_rate]
+    bars_true_pos = [int(third_percent_above*third_Nloops),int(fourth_percent_above*fourth_Nloops),int(sixth_percent_above*sixth_Nloops)]
+    bars_false_neg = [third_Nloops - int(third_percent_above*third_Nloops),fourth_Nloops - int(fourth_percent_above*fourth_Nloops),sixth_Nloops - int(sixth_percent_above*sixth_Nloops)]
 
-    bc(args.datadate,args.subfolder,dark_rate,args.samplerate)
+    bc(args.datadate,args.subfolder,dark_rate,args.samplerate,bars_true_pos,bars_false_neg)
