429202ebe5d946c76ef8712955f586d0642f6dbd
Timothy Emeigh <timothyre99@gmail.com>
2019-08-02 10:52:11 -0400

    Fixed for new Trigger Study

--- Studies/Trigger/bar_chart.py
+++ Studies/Trigger/bar_chart.py
@@ -7,7 +7,7 @@ import numpy as np
 import os
 
 #plotting data
-def bar_chart(datadate,subfolder,dark_rate,samplerate):
+def bar_chart(datadate,subfolder,dark_rate,samplerate,bars_true_pos,bars_false_neg):
     savedir = 'G:/data/watchman/'+datadate+'_watchman_spe/studies/trigger/' + samplerate + '/images/'      #sets up name for directory to save images to
     #creates directory if it doesn't exist
     if not os.path.exists(savedir):
@@ -18,16 +18,10 @@ def bar_chart(datadate,subfolder,dark_rate,samplerate):
     true_positives_third = len(os.listdir('G:/data/watchman/'+datadate+'_watchman_spe/studies/trigger/' + samplerate + '/third/'+subfolder+'/true_positives'))
     false_negatives_third = len(os.listdir('G:/data/watchman/'+datadate+'_watchman_spe/studies/trigger/' + samplerate + '/third/'+subfolder+'/false_negatives'))
 
-    true_positives_fourth = len(os.listdir('G:/data/watchman/'+datadate+'_watchman_spe/studies/trigger/' + samplerate + '/fourth/'+subfolder+'/true_positives'))
-    false_negatives_fourth = len(os.listdir('G:/data/watchman/'+datadate+'_watchman_spe/studies/trigger/' + samplerate + '/fourth/'+subfolder+'/false_negatives'))
-
-    true_positives_sixth = len(os.listdir('G:/data/watchman/'+datadate+'_watchman_spe/studies/trigger/' + samplerate + '/sixth/'+subfolder+'/true_positives'))
-    false_negatives_sixth = len(os.listdir('G:/data/watchman/'+datadate+'_watchman_spe/studies/trigger/' + samplerate + '/sixth/'+subfolder+'/false_negatives'))
-
     #sets bar width and creates bar value arrays
     bar_width = 0.4
-    bars_true_pos = [true_positives_third,true_positives_fourth,true_positives_sixth]
-    bars_false_neg = [false_negatives_third,false_negatives_fourth,false_negatives_sixth]
+    bars_true_pos.insert(0,true_positives_third)
+    bars_false_neg.insert(0,false_negatives_third)
 
     #creates x-axis locations
     ind = np.arange(len(bars_true_pos))
@@ -35,8 +29,8 @@ def bar_chart(datadate,subfolder,dark_rate,samplerate):
     #creates actual bars using values from above
     plt.rcParams.update({'font.size': 14})
     fig, ax = plt.subplots()
-    rects1 = ax.bar(ind, bars_true_pos, bar_width, color = 'blue')
-    rects2 = ax.bar(ind + bar_width, bars_false_neg, bar_width, color = 'red')
+    rects1 = ax.bar(ind, bars_true_pos, bar_width, color = 'blue', log = True)
+    rects2 = ax.bar(ind + bar_width, bars_false_neg, bar_width, color = 'red', log = True)
 
     #saves proper dark rate from array
     dark_rate_third = dark_rate[0]
@@ -48,7 +42,7 @@ def bar_chart(datadate,subfolder,dark_rate,samplerate):
     ax.set_ylabel('Number of Files')
     ax.set_xlabel('Threshold',fontweight='bold')
     ax.set_xticks(ind + 0.5*bar_width)
-    ax.set_xticklabels(('One Third Mean Peak','One Fourth Mean Peak','One Sixth Mean Peak'))
+    ax.set_xticklabels(('One Third Experimental','One Third Predicted','One Fourth Predicted','One Sixth Predicted'))
 
     #creates legend
     ax.legend((rects1[0],rects2[0]),('\nDetected\nSPEs\n','\nMissed\nSPEs\n'),loc='center left',bbox_to_anchor=(1,0.5))
@@ -76,6 +70,8 @@ if __name__ == '__main__':
     parser.add_argument('--subfolder',type = str,help = 'how much the rise time was altered', default = 'raw')
     parser.add_argument('--dark_rate',type = list,help = 'occurence rate of noise being detected as spe, format = [1/3, 1/4, 1/6]', default = ['5 Hertz','10 Hertz','20 Hertz'])
     parser.add_argument('--samplerate',type = str,help = 'downsampled rate to analyze (1 Gsps, 500 Msps, 250 Msps, 125 Msps)',default = '1 Gsps')
+    parser.add_argument('--bars_true_pos',type = list,help = 'predicted number of true positives, format = [1/3, 1/4, 1/6]',default = [1,2,3])
+    parser.add_argument('--bars_false_neg',type = str,help = 'predicted number of false negatives, format = [1/3, 1/4, 1/6]',default = [1,2,3])
     args = parser.parse_args()
 
-    bar_chart(args.datadate,args.subfolder,args.dark_rate,args.samplerate)
+    bar_chart(args.datadate,args.subfolder,args.dark_rate,args.samplerate,args.bars_true_pos,args.bars_false_neg)
