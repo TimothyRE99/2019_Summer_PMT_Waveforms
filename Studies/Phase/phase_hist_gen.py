743a9de86680bcf7847997ac47dcc436154b00b5
Timothy Emeigh <timothyre99@gmail.com>
2020-04-09 15:58:56 -0400

    Updated for finalization of correction process

--- Studies/Phase/phase_hist_gen.py
+++ Studies/Phase/phase_hist_gen.py
@@ -74,14 +74,17 @@ def phase_hist_gen(samplerate,samplerate_name,shaping,datadate,n_box,n_delay,n_a
     y = []
     x_bins = []
     median_array = []
+    correction = []
+    correction_median_array = []
     for i in range(len(phase_array)):
         filedir = 'G:/data/watchman/'+str(datadate)+'_watchman_spe/studies/phase/'+samplerate_name+'/phase='+str(phase_array[i])+'/phase_'+shaping+'/'
         Nloops = len(os.listdir(filedir))
         y_j = []
         x_j = []
+        correction_j = []
         for j in range(Nloops):
-            print(samplerate_name+';'+str(n_box)+','+str(n_delay)+','+str(n_att)+';'+str(i)+','+str(j))
-            filename = filename = filedir + 'Phase--waveforms--%05d.txt' % j
+            print('Uncorrected Files '+samplerate_name+';'+str(n_box)+','+str(n_delay)+','+str(n_att)+';'+str(i)+','+str(j))
+            filename = filedir + 'Phase--waveforms--%05d.txt' % j
             (t,v,_) = rw(filename,numhead)
             t_avg,v_avg = boxcar_wf(t,v,n_box)
             v_delay = delay_wf(v_avg,n_delay)
@@ -90,18 +93,27 @@ def phase_hist_gen(samplerate,samplerate_name,shaping,datadate,n_box,n_delay,n_a
             t_cross,_,_ = zc_locator(t_avg,v_sum)
             y_j.append(t_cross)
             x_j.append(-1*i*phase_time)
+            correction_j.append(-1*i*phase_time - t_cross)
         median_array.append(np.median(np.asarray(y_j)))
+        correction_median_array.append(np.median(np.asarray(correction_j)))
         y = y + y_j
         x = x + x_j
+        correction = correction + correction_j
         x_bins.append(-1*(i-0.5)*phase_time)
     y = np.asarray(y)
     y = y - median_array[0]
+    correction = np.asarray(correction)
+    correction = correction + median_array[0]
+    correction_median_array = np.asarray(correction_median_array)
+    correction_median_array = correction_median_array + median_array[0]
     median_array = np.asarray(median_array)
     median_array = median_array - median_array[0]
     ybin = 1/(2*samplerate)
     y_bins = np.linspace(-2*ybin,0,num=maxphase,endpoint = True)
+    y_bins_corrections = np.linspace(-ybin,ybin,num=maxphase,endpoint = True)
     x_bins = np.asarray(x_bins)
     x_bins = np.flip(x_bins)
+
     fig,ax = plt.subplots()
     h = ax.hist2d(x,y,bins = [x_bins,y_bins])
     plt.colorbar(h[3],ax = ax)
@@ -119,6 +131,89 @@ def phase_hist_gen(samplerate,samplerate_name,shaping,datadate,n_box,n_delay,n_a
     savename = savedir + filename
     fig.savefig(savename,dpi = 500)
     plt.close()
+
+    fig,ax = plt.subplots()
+    h = ax.hist2d(x,correction,bins = [x_bins,y_bins_corrections])
+    plt.colorbar(h[3],ax = ax)
+    ax.plot(np.flip(x_bins)-0.5*phase_time,correction_median_array,c = 'Green')
+    ax.set_title('Timing Corrections vs. True Timing')
+    ax.set_xlabel('True Timing (Seconds)')
+    ax.set_ylabel('Timing Corrections (Seconds)')
+    plt.get_current_fig_manager().window.showMaximized()
+    plt.show(block = False)
+    savedir = 'G:/data/watchman/'+str(datadate)+'_watchman_spe/studies/phase/Histograms/'
+    if not os.path.exists(savedir):
+        os.makedirs(savedir)
+    filename = samplerate_name+'_'+shaping+'_nbox='+str(n_box)+'_ndel='+str(n_delay)+'_natt='+str(n_att)+'_corrections.png'
+    savename = savedir + filename
+    fig.savefig(savename,dpi = 500)
+    plt.close()
+
+    y_corrected = []
+    corrected_median_array = []
+    corrected_corrections = []
+    corrected_correction_median_array = []
+    for i in range(len(phase_array)):
+        filedir = 'G:/data/watchman/'+str(datadate)+'_watchman_spe/studies/phase/'+samplerate_name+'/phase='+str(phase_array[i])+'/phase_'+shaping+'/'
+        Nloops = len(os.listdir(filedir))
+        corrected_corrections_j = []
+        y_corrected_j = []
+        for j in range(Nloops):
+            print('Corrected Files '+samplerate_name+';'+str(n_box)+','+str(n_delay)+','+str(n_att)+';'+str(i)+','+str(j))
+            filename = filedir + 'Phase--waveforms--%05d.txt' % j
+            (t,v,_) = rw(filename,numhead)
+            t_avg,v_avg = boxcar_wf(t,v,n_box)
+            v_delay = delay_wf(v_avg,n_delay)
+            v_att = attenuate_wf(v_avg,n_att)
+            v_sum = sum_wf(v_att,v_delay)
+            t_cross,_,_ = zc_locator(t_avg,v_sum)
+            t_corrected = t_cross - median_array + correction_median_array[i]
+            y_corrected_j.append(t_corrected)
+            corrected_correction = -1*i*phase_time - t_corrected
+            corrected_corrections_j.append(corrected_correction)
+        corrected_median_array.append(np.median(np.asarray(y_corrected_j)))
+        corrected_correction_median_array.append(np.median(np.asarray(corrected_corrections_j)))
+        y_corrected = y_corrected + y_corrected_j
+        corrected_corrections = corrected_corrections + corrected_corrections_j
+    y_corrected = np.asarray(y_corrected)
+    corrected_correction_median_array = np.asarray(corrected_correction_median_array)
+    corrected_corrections = np.asarray(corrected_corrections)
+
+    fig,ax = plt.subplots()
+    h = ax.hist2d(x,y_corrected,bins = [x_bins,y_bins])
+    plt.colorbar(h[3],ax = ax)
+    ax.plot(np.flip(x_bins)-0.5*phase_time,corrected_median_array,c = 'Green')
+    ax.plot(np.flip(x_bins)-0.5*phase_time,np.flip(x_bins)-0.5*phase_time,c = 'Green',ls = '--')
+    ax.set_title('Measured vs. True Timing')
+    ax.set_xlabel('True Timing (Seconds)')
+    ax.set_ylabel('Measured Timing (Seconds)')
+    plt.get_current_fig_manager().window.showMaximized()
+    plt.show(block = False)
+    savedir = 'G:/data/watchman/'+str(datadate)+'_watchman_spe/studies/phase/Histograms/'
+    if not os.path.exists(savedir):
+        os.makedirs(savedir)
+    filename = 'corrected '+samplerate_name+'_'+shaping+'_nbox='+str(n_box)+'_ndel='+str(n_delay)+'_natt='+str(n_att)+'.png'
+    savename = savedir + filename
+    fig.savefig(savename,dpi = 500)
+    plt.close()
+
+    fig,ax = plt.subplots()
+    h = ax.hist2d(x,corrected_corrections,bins = [x_bins,y_bins_corrections])
+    plt.colorbar(h[3],ax = ax)
+    ax.plot(np.flip(x_bins)-0.5*phase_time,corrected_correction_median_array,c = 'Green')
+    ax.set_title('Timing Corrections vs. True Timing')
+    ax.set_xlabel('True Timing (Seconds)')
+    ax.set_ylabel('Timing Corrections (Seconds)')
+    plt.get_current_fig_manager().window.showMaximized()
+    plt.show(block = False)
+    savedir = 'G:/data/watchman/'+str(datadate)+'_watchman_spe/studies/phase/Histograms/'
+    if not os.path.exists(savedir):
+        os.makedirs(savedir)
+    filename = 'corrected '+samplerate_name+'_'+shaping+'_nbox='+str(n_box)+'_ndel='+str(n_delay)+'_natt='+str(n_att)+'_corrections.png'
+    savename = savedir + filename
+    fig.savefig(savename,dpi = 500)
+    plt.close()
+
     return()
 
 #main function
@@ -128,19 +223,5 @@ if __name__ == '__main__':
     parser.add_argument('--datadate',type = str,help = 'date when data was gathered, YYYYMMDD', default = '20190724')
     parser.add_argument('--numhead',type=int,help='number of lines to ignore for header',default = 5)
     args = parser.parse_args()
-
-    #for n_box in range(5):
-    #    if n_box == 3:
-    #        pass
-    #    else:
-    #        for n_delay in range(1,17):
-    #            if n_delay != 1 and n_delay != 2 and n_delay != 4 and n_delay != 8 and n_delay !=  16:
-    #                pass
-    #            else:
-    #                for n_att in range(1,5):
-    #                    if n_att == 3:
-    #                        pass
-    #                    else:
-    #                        phase_hist_gen(250000000,'250 Msps','raw_gained_analyzed',args.datadate,n_box,n_delay,n_att,args.numhead)
 
     phase_hist_gen(250000000,'250 Msps','raw_gained_analyzed',args.datadate,2,1,2,args.numhead)
