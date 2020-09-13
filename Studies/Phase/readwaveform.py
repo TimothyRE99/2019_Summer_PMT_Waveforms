0ced626eebd5cd27dd02564a2ad75fe6e2340842
Timothy Emeigh <timothyre99@gmail.com>
2020-09-13 17:52:27 -0400

    Set to get downsampled waveforms

--- Studies/Phase/readwaveform.py
+++ Studies/Phase/readwaveform.py
@@ -24,11 +24,18 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(prog="read waveform",description="read the waveform datafile.")
     parser.add_argument("--numhead",type=int,help='number of header lines to skip in the raw file',default=5)
-    parser.add_argument("--filename",type=str,help="filename",default='g:/data/watchman/20190724_watchman_spe/C2--waveforms--00000.txt')
+    parser.add_argument("--filename",type=str,help="filename",default='G:/data/watchman/20190724_watchman_spe/studies/phase/250 Msps/phase=0/phase_raw_gained_analyzed/Phase--waveforms--00000.txt')
     args = parser.parse_args()
 
     x,y,header = read_waveform(args.filename,args.numhead)
     print(header)
-    plt.plot(x,y)
+    FontSize = 32
+    plt.rcParams.update({'font.size': FontSize})
+    _,ax = plt.subplots()
+    ax.plot(x,y,linewidth=4)
+    ax.scatter(x,y,linewidth=4)
+    ax.set_xlabel('Time (s)')
+    ax.set_ylabel('Bits')
+    ax.set_title('250 MSPS Sample Waveform')
     plt.get_current_fig_manager().window.showMaximized()        #maximizes plot
     plt.show()
