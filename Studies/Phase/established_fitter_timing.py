2a2d95a4692b3ab6cb9a141cc62e69397cc7cdd6
Timothy Emeigh <timothyre99@gmail.com>
2020-10-29 14:08:57 -0400

    Versions for just template

---
+++ Studies/Phase/established_fitter_timing.py
@@ -0,0 +1,162 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+import os
+import random
+import math
+from readwaveform import read_waveform as rw
+from writewaveform import write_waveform as ww
+import scipy.interpolate as it
+from scipy.optimize import curve_fit as cf
+from unispline import unispline as us
+
+def gauss_histogram(histo):
+    histo = np.sort(histo)
+    #splitting off array into upper and lower halves
+    histo_low = np.array_split(histo,2)[0]
+    histo_high = np.array_split(histo,2)[1]
+    #determining median of each half (aka, 1st and 3rd quartiles)
+    medi_low = np.median(histo_low)
+    medi_high = np.median(histo_high)
+    iqr = (medi_high - medi_low)                    #calculating inter-quartile range
+    #calculating outlier thresholds
+    out_low = (medi_low - 1.5*iqr)
+    out_high = (medi_low + 1.5*iqr)
+    histo_out_remove = histo[np.where((out_low <= histo) & (histo <= out_high))]    #creating new array with outliers removed
+    #determining mean and std guesses for central data
+    histo_mean = np.mean(histo_out_remove)
+    histo_std = np.std(histo_out_remove)
+    return (histo_mean,histo_std)
+
+def fit_function(x,B,mu,sigma):
+    #funcion for a gaussian scaled by factor B
+    return (B * (1/np.sqrt(2 * np.pi * sigma**2)) * np.exp(-1.0 * (x - mu)**2 / (2 * sigma**2)))
+
+def timing_extraction(t_fitter,v_fitter):
+    v_fitter_max = np.amax(v_fitter)
+    v_fitter_max_index = np.where(v_fitter == v_fitter_max)[0][0]
+    v_fitter = v_fitter[0:v_fitter_max_index]
+    v_fitter_zero = v_fitter_max/2
+    v_fitter_zero_array = np.abs(v_fitter - v_fitter_zero)
+    v_fitter_zero_index = np.where(v_fitter_zero_array == np.amin(v_fitter_zero_array))[0]
+    t_cross = t_fitter[v_fitter_zero_index]
+    return(t_cross)
+
+def chi_squared(observed,expected):
+    chi2 = np.sum(np.square(observed - expected))
+    return(chi2)
+
+def fitter_timing(datadate,numhead,samplerate,samplerate_name,shaping):
+    if samplerate_name == 'INVALID':
+        return("Failed")
+    phase_time = 1/20000000000
+    maxphase = int(20000000000/samplerate + 0.5)
+    t_fitter,v_fitter,_ = rw('G:/data/watchman/'+datadate+'_watchman_spe/d2/d2_average.txt',1)
+    uspl = us(t_fitter,v_fitter)
+    filedir = 'G:/data/watchman/'+str(datadate)+'_watchman_spe/studies/phase/'+samplerate_name+'/file_template/'
+    Nloops = len(os.listdir(filedir + 'phase=0/phase_'+shaping))
+    difference_list = []
+    chi_list = []
+    for i in range(Nloops):
+        print(i)
+        j = random.randint(0,maxphase - 1)
+        filename = filedir + 'phase='+str(j)+'/phase_'+shaping+'/Phase--waveforms--%05d.txt' % i
+        t,v,_ = rw(filename,5)
+        if shaping == 'raw_gained_analyzed_peaked':
+            v = -1*v
+        ET = t[0:10]
+        EV = v[0:10]
+        chi2_min = -1
+        x_min = -1
+        y_min = -1
+        shift_min = -1
+        shifts = np.arange(0,80/20000000000,1e-11)
+        for shift in shifts:
+            pre_OV = uspl(ET + shift)
+            A = np.array([[np.sum(np.square(pre_OV)),np.sum(pre_OV),np.sum(np.multiply(pre_OV,EV))],[np.sum(pre_OV),len(EV),np.sum(EV)]])
+            A[0] = A[0]/A[0][0]
+            A[1] = np.subtract(A[1],A[1][0]*A[0])
+            A[1] = A[1]/A[1][1]
+            A[0] = np.subtract(A[0],A[0][1]*A[1])
+            x = A[0][2]
+            y = A[1][2]
+            OV = x*pre_OV + y
+            chi2 = chi_squared(OV,EV)
+            if chi2_min < 0:
+                shift_min = shift
+                chi2_min = chi2
+                x_min = x
+                y_min = y
+            elif chi2 < chi2_min:
+                shift_min = shift
+                chi2_min = chi2
+                x_min = x
+                y_min = y
+        v_fit = x_min*uspl(t_fitter + shift_min) + y_min
+        t_cross = timing_extraction(t_fitter,v_fit)
+        difference_list.append((-1*j*phase_time - t_cross)[0])
+        chi_list.append(chi2_min)
+    difference_list = np.asarray(difference_list)
+    chi_list = np.asarray(chi_list)
+    true_mean = '%5g' % (np.mean(difference_list)*1e12)
+    true_std = '%5g' % (np.std(difference_list)*1e12)
+    bins = np.linspace(-2.2e-9,2.2e-9,num = 100)
+    histo_data, bins_data = np.histogram(difference_list, bins = bins)
+    binwidth = (bins_data[1] - bins_data[0])
+    binscenters = np.array([0.5 * (bins_data[i] + bins_data[i+1]) for i in range(len(bins_data)-1)])
+    FontSize = 32
+    plt.rcParams.update({'font.size': FontSize})
+    _,ax = plt.subplots()
+    ax.bar(binscenters, histo_data, width=binwidth)
+    ax.set_xlabel('True Timing - Recovered Timing')
+    ax.set_ylabel('Count')
+    ax.set_title(samplerate_name+' - Fitter Timings')
+    ax.text(0.05, 0.95, 'Distribution Parameters:\nMean: '+true_mean+' ps\nStandard Deviation: '+true_std+' ps', transform=ax.transAxes, fontsize=FontSize, verticalalignment='top', bbox=dict(boxstyle='round', facecolor='White', alpha=0.5))
+    plt.get_current_fig_manager().window.showMaximized()
+    plt.show()
+    plt.close()
+
+    bins = np.linspace(0,30200,num = 100)
+    histo_data, bins_data = np.histogram(chi_list, bins = bins)
+    binwidth = (bins_data[1] - bins_data[0])
+    binscenters = np.array([0.5 * (bins_data[i] + bins_data[i+1]) for i in range(len(bins_data)-1)])
+    plt.rcParams.update({'font.size': FontSize})
+    plt.bar(binscenters, histo_data, width=binwidth, log=True)
+    plt.xlabel('Chi Squared')
+    plt.ylabel('Count')
+    plt.title(samplerate_name+' - Chi Squared Counts - Fitter Timings')
+    plt.get_current_fig_manager().window.showMaximized()
+    plt.show()
+    plt.close()
+
+    _,ax = plt.subplots()
+    h = ax.hist2d(difference_list,chi_list,bins=65,norm = LogNorm())
+    plt.colorbar(h[3],ax = ax)
+    ax.set_title(samplerate_name+' - Chi Squared vs. Timing Corrections - Fitter Timings')
+    ax.set_xlabel('Timing Corrections')
+    ax.set_ylabel('Chi Squared')
+    plt.get_current_fig_manager().window.showMaximized()
+    plt.show()
+    plt.close()
+    return("Passed")
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(prog="fitter timing",description="Uses average waveform to fit for timing")
+    parser.add_argument('--datadate',type = str,help = 'date when data was gathered, YYYYMMDD', default = '20190724')
+    parser.add_argument('--numhead',type=int,help='number of lines to ignore for header',default = 5)
+    parser.add_argument('--samplerate',type=int,help='samples per second',default = 250000000)
+    parser.add_argument('--shaping',type=str,help='name of shaping',default = 'raw_gained_analyzed_noised')
+    args = parser.parse_args()
+
+    if args.samplerate == 250000000:
+        samplerate_name = '250 Msps'
+    elif args.samplerate == 500000000:
+        samplerate_name = '500 Msps'
+    elif args.samplerate == 1000000000:
+        samplerate_name = '1 Gsps'
+    else:
+        samplerate_name = 'INVALID'
+    
+    status = fitter_timing(args.datadate,args.numhead,args.samplerate,samplerate_name,args.shaping)
+    print(status)
---
+++ Studies/Phase/established_timing_resolution_compare.py
@@ -0,0 +1,188 @@
+#import necessary
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+from readwaveform import read_waveform as rw
+import random
+from scipy.optimize import curve_fit as cf
+
+def gauss_histogram(histo):
+    histo = np.sort(histo)
+    #splitting off array into upper and lower halves
+    histo_low = np.array_split(histo,2)[0]
+    histo_high = np.array_split(histo,2)[1]
+    #determining median of each half (aka, 1st and 3rd quartiles)
+    medi_low = np.median(histo_low)
+    medi_high = np.median(histo_high)
+    iqr = (medi_high - medi_low)                    #calculating inter-quartile range
+    #calculating outlier thresholds
+    out_low = (medi_low - 1.5*iqr)
+    out_high = (medi_low + 1.5*iqr)
+    histo_out_remove = histo[np.where((out_low <= histo) & (histo <= out_high))]    #creating new array with outliers removed
+    #determining mean and std guesses for central data
+    histo_mean = np.mean(histo_out_remove)
+    histo_std = np.std(histo_out_remove)
+    return (histo_mean,histo_std)
+
+def fit_function(x,B,mu,sigma):
+    #funcion for a gaussian scaled by factor B
+    return (B * (1/np.sqrt(2 * np.pi * sigma**2)) * np.exp(-1.0 * (x - mu)**2 / (2 * sigma**2)))
+
+#runs boxcar averaging algorithm
+def boxcar_wf(t,v,n):
+    if(n==0):       #returns standard values if n = 0
+        return t,v
+    #reduces length of array by n if n > 0
+    v1 = np.zeros(len(v)-n)
+    t1 = np.zeros(len(t)-n)
+    #runs boxcar moving average on array
+    for i in range(len(v1)): 
+        vsum = 0
+        for j in range(n):
+            vsum += v[n+i-j]
+        v1[i]=float(vsum)/float(n)
+        t1[i] = t[i] 
+    return t1,v1
+
+#runs delay of waveform
+def delay_wf(v,n):
+    #pads beginning of array with n 0's and removes n final indices
+    v_insert = np.zeros(n)
+    v1 = np.insert(v,0,v_insert)[:-n]
+    return v1
+
+#runs inversion and attenuation of waveform
+def attenuate_wf(v,n):
+    v1 = -1 * v     #inverts waveform
+    #doesn't attenuate if n = 1
+    if n == 1:
+        return v1
+    #divides by n otherwise
+    v1 = v1 / n
+    return v1
+
+#sums waveforms together
+def sum_wf(v_att,v_delay):
+    v_sum = np.add(v_att,v_delay)       #adds waveforms together
+    return v_sum
+
+def zc_locator(t,v):
+    stop_ind = int(len(v)/3)
+    v_norm = v/max(v[0:stop_ind])     #normalizes for easy checking
+    #creates array of "True" and "False" entries for where condition is met
+    checkPeak = v_norm == 1
+    checkCross = v_norm <= 0
+    #turns into array of indices each value held above
+    indexPeak = np.asarray([k for k, x in enumerate(checkPeak) if x])
+    indexCross = np.asarray([k for k, x in enumerate(checkCross) if x])
+    index_Peak = indexPeak[0]       #creates peak index into int
+    #establishes first crossed index after peak
+    indexCross_removed = indexCross[np.where(indexCross > index_Peak)]
+    index_Cross = indexCross_removed[0]
+    #interpolates time of crossing
+    t_bef = t[index_Cross - 1]
+    t_aft = t[index_Cross]
+    v_bef = v[index_Cross - 1]
+    v_aft = v[index_Cross]
+    slope = (v_aft - v_bef) / (t_aft - t_bef)
+    t_pass = (-1 * v_bef) / slope
+    t_cross = t_bef + t_pass
+    return (t_cross,index_Cross,index_Peak)
+
+def phase_hist_gen(samplerate,samplerate_name,shaping,datadate,n_box,n_delay,n_att,numhead):
+    phase_time = 1/20000000000
+    maxphase = int(20000000000/samplerate + 0.5)
+    phase_array = np.arange(0,maxphase)
+    median_array = []
+    correction_median_array = []
+    for i in range(len(phase_array)):
+        filedir = 'G:/data/watchman/'+str(datadate)+'_watchman_spe/studies/phase/'+samplerate_name+'/file_template/phase='+str(phase_array[i])+'/phase_'+shaping+'/'
+        Nloops = len(os.listdir(filedir))
+        y_j = []
+        correction_j = []
+        for j in range(Nloops):
+            print(samplerate_name+';'+str(n_box)+','+str(n_delay)+','+str(n_att)+';'+str(i)+','+str(j))
+            filename = filedir + 'Phase--waveforms--%05d.txt' % j
+            (t,v,_) = rw(filename,numhead)
+            if shaping == 'raw_gained_analyzed_noised':
+                v = v*-1
+            t_avg,v_avg = boxcar_wf(t,v,n_box)
+            v_delay = delay_wf(v_avg,n_delay)
+            v_att = attenuate_wf(v_avg,n_att)
+            v_sum = sum_wf(v_att,v_delay)
+            t_cross,_,_ = zc_locator(t_avg,v_sum)
+            y_j.append(t_cross)
+            correction_j.append(-1*i*phase_time - t_cross)
+        median_array.append(np.median(np.asarray(y_j)))
+        correction_median_array.append(np.median(np.asarray(correction_j)))
+    correction_median_array = np.asarray(correction_median_array)
+    correction_median_array = correction_median_array + median_array[0]
+
+    difference_list = []
+    corrected_difference_list = []
+    for j in range(Nloops):
+        print(j)
+        i = random.randint(0,maxphase - 1)
+        filedir = 'G:/data/watchman/'+str(datadate)+'_watchman_spe/studies/phase/'+samplerate_name+'/file_template/phase='+str(i)+'/phase_'+shaping+'/'
+        filename = filedir + 'Phase--waveforms--%05d.txt' % j
+        (t,v,_) = rw(filename,numhead)
+        if shaping == 'raw_gained_analyzed_noised':
+            v = v*-1
+        t_avg,v_avg = boxcar_wf(t,v,n_box)
+        v_delay = delay_wf(v_avg,n_delay)
+        v_att = attenuate_wf(v_avg,n_att)
+        v_sum = sum_wf(v_att,v_delay)
+        t_cross,_,_ = zc_locator(t_avg,v_sum)
+        t_cross = t_cross - median_array[0]
+        difference_list.append(-1*i*phase_time - t_cross)
+        corrected_difference_list.append(-1*i*phase_time - (t_cross + correction_median_array[i]))
+
+    difference_list = np.asarray(difference_list)
+    corrected_difference_list = np.asarray(corrected_difference_list)
+
+    FontSize = 32
+    plt.rcParams.update({'font.size': FontSize})
+    histo_mean,histo_std = gauss_histogram(difference_list)
+    difference_list = difference_list[(difference_list >= histo_mean - 5*histo_std) & (difference_list <= histo_mean + 5*histo_std)]
+    true_mean = '%5g' % (np.mean(difference_list)*1e12)
+    true_std = '%5g' % (np.std(difference_list)*1e12)
+    bins = 100#np.linspace(-2.2e-9,2.2e-9,num = 100)
+    histo_data, bins_data = np.histogram(difference_list, bins = bins)
+    binwidth = (bins_data[1] - bins_data[0])
+    binscenters = np.array([0.5 * (bins_data[i] + bins_data[i+1]) for i in range(len(bins_data)-1)])
+    _,ax = plt.subplots()
+    ax.bar(binscenters, histo_data, width=binwidth)
+    ax.set_xlabel('True Timing - Recovered Timing')
+    ax.set_ylabel('Count')
+    ax.set_title(samplerate_name+' - CFD Timings, Uncorrected')
+    ax.text(0.025, 0.95, 'Distribution Parameters:\nMean: '+true_mean+' ps\nStandard Deviation: '+true_std+' ps', transform=ax.transAxes, fontsize=FontSize, verticalalignment='top', bbox=dict(boxstyle='round', facecolor='White', alpha=0.5))
+    plt.get_current_fig_manager().window.showMaximized()
+    plt.show()
+
+    histo_mean,histo_std = gauss_histogram(corrected_difference_list)
+    corrected_difference_list = corrected_difference_list[(corrected_difference_list >= histo_mean - 10*histo_std) & (corrected_difference_list <= histo_mean + 10*histo_std)]
+    true_mean = '%5g' % (np.mean(corrected_difference_list)*1e12)
+    true_std = '%5g' % (np.std(corrected_difference_list)*1e12)
+    bins = 100#np.linspace(-2.2e-9,2.2e-9,num = 100)
+    histo_data, bins_data = np.histogram(corrected_difference_list, bins = bins)
+    binwidth = (bins_data[1] - bins_data[0])
+    binscenters = np.array([0.5 * (bins_data[i] + bins_data[i+1]) for i in range(len(bins_data)-1)])
+    _,ax = plt.subplots()
+    ax.bar(binscenters, histo_data, width=binwidth)
+    ax.set_xlabel('True Timing - Recovered Timing')
+    ax.set_ylabel('Count')
+    ax.set_title(samplerate_name+' - CFD Timings')
+    ax.text(0.025, 0.95, 'Distribution Parameters:\nMean: '+true_mean+' ps\nStandard Deviation: '+true_std+' ps', transform=ax.transAxes, fontsize=FontSize, verticalalignment='top', bbox=dict(boxstyle='round', facecolor='White', alpha=0.5))
+    plt.get_current_fig_manager().window.showMaximized()
+    plt.show()
+
+#main function
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(prog="timing_resolution_compare",description="Generates 1D Histograms of timing resolution with random sampled phase.")
+    parser.add_argument('--datadate',type = str,help = 'date when data was gathered, YYYYMMDD', default = '20190724')
+    parser.add_argument('--numhead',type=int,help='number of lines to ignore for header',default = 5)
+    args = parser.parse_args()
+
+    phase_hist_gen(250000000,'250 Msps','raw_gained_analyzed_peaked',args.datadate,2,1,2,args.numhead)
