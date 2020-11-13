9caeaaed0ec64ca02ae5ae235e0777230fda1d92
Timothy Emeigh <timothyre99@gmail.com>
2020-11-13 13:00:16 -0500

    Minor Fixes

--- Studies/Phase/established_phase_hist_gen.py
+++ Studies/Phase/established_phase_hist_gen.py
@@ -222,7 +222,7 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(prog="phase_hist_gen",description="Generates 2D Histograms of timing resolution vs. phase.")
     parser.add_argument('--datadate',type = str,help = 'date when data was gathered, YYYYMMDD', default = '20190724')
-    parser.add_argument('--numhead',type=int,help='number of lines to ignore for header',default = 5)
+    parser.add_argument('--numhead',type=int,help='number of lines to ignore for header',default = 1)
     args = parser.parse_args()
 
     phase_hist_gen(250000000,'250 Msps','raw_gained_analyzed_noised',args.datadate,2,1,2,args.numhead)
