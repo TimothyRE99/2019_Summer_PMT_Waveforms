f74e8fcfae28a6fa7e2d17d1ddfa9d910272db61
Timothy Emeigh <timothyre99@gmail.com>
2021-05-10 15:15:33 -0400

    Removing unnecessary Files

--- enumerate_test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-y = np.array([1,2,3,4,5,6,7,8,9,10,9,8,7,6,5,4,3,2,1])
-check = y >= 5
-print(check)
-index = [k for k, x in enumerate(check) if x]
-print(index)
-index_last = int(index[len(index)-1])
-print(index_last)
--- namecheck.py
+++
@@ -1,4 +0,0 @@
-import os
-
-files = os.listdir('G:/data/watchman/20190516_watchman_spe/d1/d1_raw')
-print(files[3310])
